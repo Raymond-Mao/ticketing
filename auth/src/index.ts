d3692131366425afcf8b6c3ac3746de3a981e7d4
Raymond <563822080@qq.com>
2022-10-11 21:06:40 +1300

    add ingress yaml for the service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,11 @@ import { json } from "body-parser";
 
 const app = express();
 app.use(json());
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hi there");
+});
+
 app.listen(3000, () => {
   console.log("listening on 3000");
 });
