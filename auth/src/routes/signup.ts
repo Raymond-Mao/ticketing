b1bf5f9fd9197f621ac5388bccb7cac268a81330
Raymond <563822080@qq.com>
2022-10-14 12:38:14 +1300

    add env config for jwt-key & install jwt lib

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user";
 import { BadRequestError, RequestValidationError } from "../errors";
 const router = express.Router();
@@ -24,6 +25,18 @@ router.post(
     }
     const user = User.build({ email, password });
     await user.save();
+    //generate jwt
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+    // store it to session object
+    req.session = {
+      jwt: userJwt,
+    };
     res.status(201).send(user);
   }
 );
