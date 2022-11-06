869a4cadf19a913e378c2d3329b6654b16095e7e
Raymond <563822080@qq.com>
2022-11-06 13:01:54 +1300

    add abstract class for natstream service

--- common/build/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./errors";
-export * from "./middlewares/current-user";
-export * from "./middlewares/error-handler";
-export * from "./middlewares/require-auth";
-export * from "./middlewares/validate-request";
