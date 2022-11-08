ed34dfa848acdce6e426d9466f1e4d267a251949
Raymond <563822080@qq.com>
2022-11-08 21:35:41 +1300

    create 2 models for order service

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,19 +1,18 @@
 import mongoose from "mongoose";
 import { OrderStatus } from "@arale-auth/common";
+import { TicketDoc } from "./ticket";
 interface OrderAttrs {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
-
 interface OrderDoc extends mongoose.Document {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
-
 interface OrderModel extends mongoose.Model<OrderDoc> {
   build(attrs: OrderAttrs): OrderDoc;
 }
@@ -27,6 +26,8 @@ const orderSchema = new mongoose.Schema(
     status: {
       type: String,
       required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
     },
     expiresAt: {
       type: mongoose.Schema.Types.Date,
