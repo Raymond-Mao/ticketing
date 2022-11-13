8a2e21c308023b63360cf2e216ca9258da302f86
Raymond <563822080@qq.com>
2022-11-13 17:21:55 +1300

    add order service with exp listener & test case

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -9,6 +9,8 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("Waiting this many milliseconds to process the job:", delay);
+
     await expirationQueue.add(
       {
         orderId: data.id,
@@ -17,6 +19,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
         delay,
       }
     );
+
     msg.ack();
   }
 }
