f5006f96306a6e6d1b089ef2cf8d48ff6bdbadec
Raymond <563822080@qq.com>
2022-11-26 16:07:15 +1300

    test on workflow of docker

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import { app } from "./app";
 import mongoose from "mongoose";
 
 const start = async () => {
-  console.log("start up...");
+  console.log("start up.");
 
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
